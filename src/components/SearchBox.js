60a622684f00fcf837abe13e6fe4eff3911899ae
Michael Pappas <mixalis.pappas23@gmail.com>
2021-05-24 21:33:13 +1000

    created a search box file

---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = (props) => {
+    return (
+        <div className="col col-sm-4">
+            <input></input>
+        </div>
+    )
+}
+
+export default SearchBox;
