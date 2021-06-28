9df9b035fd5f2c54bff12b08d45ac333482fdcd9
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-28 20:53:40 +1000

    adding more code on movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,7 +9,7 @@ const MovieList = (props) => {
 				<div className='image-container d-flex justify-content-start m-3'>
 					<img src={movie.Poster} alt='movie'></img>
 					<div
-						onClick={() => props.handleFavouritesClick(movie)}
+						onClick={() => props.handleFavoritesClick(movie)}
 						className='overlay d-flex align-items-center justify-content-center'
 					>
 						<FavoriteComponent />
