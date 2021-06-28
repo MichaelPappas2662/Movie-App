2e430da9658205679900e64205bc7f8409a2314e
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-28 20:53:31 +1000

    adding movies file

--- src/components/AddFavorites.js
+++ src/components/AddFavorites.js
@@ -7,7 +7,7 @@ function AddFavorites() {
             <svg 
                 xmlns="http://www.w3.org/2000/svg"
                 width="16" height="16" 
-                fill="currentColor" 
+                fill="red" 
                 class="bi bi-heart-fill" 
                 viewBox="0 0 16 16">
                 
