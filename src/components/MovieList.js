4d13ea00dc0a5031b05b3d3b8179d3821637f817
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-28 19:57:32 +1000

    adding favoriteComponent int movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-const MovieList = (props) => (
-  <>
-    {props.movies.map((movie, index) => (
-      <div className="image-container d-flex justify-content-start m-3">
-        <img src={movie.Poster} alt="movie" />
-        <div className="overlay d-flex align-items-center justify-content-center"></div>
-      </div>
-    ))}
-  </>
-);
+const MovieList = (props) => {
+	const FavoriteComponent = props.favoriteComponent;
+
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+					<div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
+						<FavoriteComponent />
+					</div>
+				</div>
+			))}
+		</>
+	);
+};
 
 export default MovieList;
