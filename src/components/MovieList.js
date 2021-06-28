88a0411ce19764080a99a12fabce1449150ae0aa
Michael Pappas <mixalis.pappas23@gmail.com>
2021-06-28 19:03:42 +1000

    adding more code on the movie list css

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const MovieList = (props) => {
-    return (
-        <>
-            {props.movies.map((movie, index) => (
-            <div className='d-flex justify-content-start m-3'>
-                <img src={movie.Poster} alt='movie' ></img>
-            </div>
-            ))}
-        </>
-    );
-};
+const MovieList = (props) => (
+  <>
+    {props.movies.map((movie, index) => (
+      <div className="image-container d-flex justify-content-start m-3">
+        <img src={movie.Poster} alt="movie" />
+        <div className="overlay d-flex align-items-center justify-content-center"></div>
+      </div>
+    ))}
+  </>
+);
 
 export default MovieList;
